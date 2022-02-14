b34e4d15a7fb1f84e2868ffb57bc7ba63de8aee3
akshxl09 <59478929+akshxl09@users.noreply.github.com>
2022-02-15 01:14:14 +0900

    2022-02-15

--- 1018_체스판 다시 칠하기.py
+++ 1018_체스판 다시 칠하기.py
@@ -8,25 +8,32 @@ row = 0
 col = 0
 answer = []
 while row < M - 7:
-    cnt = 0
+    cnt_b = 0
+    cnt_w = 0
 
     for i in range(row, row + 8):
         for j in range(col, col + 8):
-            if i==row and j==col: continue
             if (row+col)%2 == (i+j)%2:
-                if lst[row][col] != lst[i][j]:
-                    cnt += 1
+                if 'B' != lst[i][j]:
+                    cnt_b += 1
             else:
-                if lst[row][col] == lst[i][j]:
-                    cnt += 1
+                if 'B' == lst[i][j]:
+                    cnt_b += 1
+            
+            if (row+col)%2 == (i+j)%2:
+                if 'W' != lst[i][j]:
+                    cnt_w += 1
+            else:
+                if 'W' == lst[i][j]:
+                    cnt_w += 1
 
-    answer.append(cnt) 
+    answer.append(min(cnt_b, cnt_w)) 
 
     col += 1
     if col >= N - 7:
         col = 0
         row += 1
 
-print(len(answer))
-print(answer)
 print(min(answer))
+
+#w, b를 뭐로 판단하지?
